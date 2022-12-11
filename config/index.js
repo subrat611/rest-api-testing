38219e4b75426cd526ad3a63eb2171b2b7e3404a
subrat <subratj611@gmail.com>
2022-12-11 12:53:05 +0530

    body-parser and dev env setup

--- config/index.js
+++ config/index.js
@@ -2,8 +2,9 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const { APP_PORT } = process.env;
+const { APP_PORT, DEV_ENV } = process.env;
 
 module.exports = {
   APP_PORT,
+  DEV_ENV,
 };
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { APP_PORT } = require("./config");
+const { errHandler } = require("./middlewares/error.handler");
 
 const app = express();
+
+// parse application/json
+app.use(bodyParser.json());
 
 // import routes
 const routes = require("./routes");
 
 // Routes
 app.use("/api/auth", routes.authRoutes);
+
+app.use(errHandler);
 
 app.listen(APP_PORT, () => {
   console.log(`Listening on port ${APP_PORT}`);
